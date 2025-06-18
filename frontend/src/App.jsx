76fd8f7d9cac3aee4f63949e94ce428a19c97ef3
Adda <addisuanley2010@gmail.com>
2025-06-18 19:21:03 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline bg-red-600">    Hello world!  </h1>
+    </>
+  )
+}
+
+export default App
