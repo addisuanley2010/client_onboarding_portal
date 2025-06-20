4d455e8c53dee5ea0f4b6e9d8e0b960cc1a4b761
Adda <addisuanley2010@gmail.com>
2025-06-20 21:08:59 +0300

    port 3001 added

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/api/users", user);
  
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000||3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
